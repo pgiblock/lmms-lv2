dc2f69c23319fced1d6d7b4a25685ef535ba51b8
Paul Giblock <pgiblox@gmail.com>
2012-01-10 01:18:27 -0500

    Port Oscillator code from LMMS to C
    Not totally done, especially in regards to the "port" fields.
    Some of these should definitely be float* instead of float*.
    
    Also, it should be fairly easy to move some branching to outside
    the loops once we've verified correctness.

---
+++ src/oscillator.c
@@ -0,0 +1,238 @@
+/*
+ * Oscillator.cpp - implementation of powerful oscillator-class
+ *
+ * Copyright (c) 2004-2009 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of Linux MultiMedia Studio - http://lmms.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <math.h>
+#include <stdio.h>
+
+#include "oscillator.h"
+
+static Oscillator*
+osc_create(float wave_shape, float modulation_algo,
+           float freq, float detuning, float volume,
+           struct Oscillator_st* sub_osc, float phase_offset,
+           float sample_rate) {
+
+	Oscillator* o = (Oscillator*)malloc(sizeof(Oscillator));
+	if (!o) {
+		fprintf(stderr, "Could not allocate Oscillator.\n");
+		return NULL;
+	}
+
+	// FIXME: Ideally share some config across oscillators in the same instrument
+	o->wave_shape = wave_shape;
+	o->modulation_algo = modulation_algo;
+	o->freq = freq;
+	o->detuning = detuning;
+	o->volume = volume;
+	o->ext_phase_offset = phase_offset;
+	o->sub_osc = sub_osc;
+	o->phase_offset = phase_offset;
+	o->phase = phase_offset;
+	o->sample_rate = sample_rate;
+
+	return o;
+}
+
+
+static void
+osc_destroy(Oscillator* o) {
+	free(o);
+}
+
+
+// should be called every time phase-offset is changed...
+static inline void
+osc_recalc_phase(Oscillator* o) {
+	if (o->phase_offset != o->ext_phase_offset) {
+		o->phase        -= o->phase_offset;
+		o->phase_offset  = o->ext_phase_offset;
+		o->phase        += o->phase_offset;
+	}
+	// make sure we're not running negative when doing PM
+	o->phase = absFraction(o->phase) + 2;
+}
+
+
+static inline bool
+osc_sync_ok(Oscillator* o, float _osc_coeff) {
+	const float v1 = o->phase;
+	o->phase += _osc_coeff;
+
+	// check whether m_phase is in next period
+	return( floorf( o->phase ) > floorf( v1 ) );
+}
+
+
+static float
+osc_sync_init(Oscillator* o, sample_t* buff, const fpp_t len) {
+	if (o->sub_osc != NULL) {
+		osc_update(o->sub_osc, buff, len);
+	}
+	osc_recalc_phase(o);
+	return( o->freq * o->detuning );
+}
+
+
+
+// if we have no sub-osc, we can't do any modulation... just get our samples
+void osc_update_no_sub(Oscillator* o, sample_t* buff, fpp_t len) {
+	const float osc_coeff = o->freq * o->detuning;
+
+	osc_recalc_phase(o);
+
+	for (fpp_t frame = 0; frame < len; ++frame) {
+		buff[frame] = osc_get_sample(o, o->phase) * o->volume;
+		o->phase += osc_coeff;
+	}
+}
+
+
+// do pm by using sub-osc as modulator
+void osc_update_pm(Oscillator* o, sample_t* buff, fpp_t len) {
+	const float osc_coeff = o->freq * o->detuning;
+
+	osc_update(o->sub_osc, buff, len);
+	osc_recalc_phase(o);
+
+	for (fpp_t frame = 0; frame < len; ++frame) {
+		buff[frame] = osc_get_sample(o, o->phase + buff[frame]) * o->volume;
+		o->phase += osc_coeff;
+	}
+}
+
+
+// do am by using sub-osc as modulator
+void osc_update_am(Oscillator* o, sample_t* buff, fpp_t len) {
+	const float osc_coeff = o->freq * o->detuning;
+
+	osc_update(o->sub_osc, buff, len);
+	osc_recalc_phase(o);
+
+	for (fpp_t frame = 0; frame < len; ++frame) {
+		buff[frame] *= osc_get_sample(o, o->phase) * o->volume;
+		o->phase += osc_coeff;
+	}
+}
+
+
+// do mix by using sub-osc as mix-sample
+void osc_update_mix(Oscillator* o, sample_t* buff, fpp_t len) {
+	const float osc_coeff = o->freq * o->detuning;
+
+	osc_update(o->sub_osc, buff, len);
+	osc_recalc_phase(o);
+
+	for (fpp_t frame = 0; frame < len; ++frame) {
+		buff[frame] += osc_get_sample(o, o->phase) * o->volume;
+		o->phase += osc_coeff;
+	}
+}
+
+
+// sync with sub-osc (every time sub-osc starts new period, we also start new
+// period)
+void osc_update_sync(Oscillator* o, sample_t* buff, fpp_t len) {
+	const float osc_coeff = o->freq * o->detuning;
+	const float sub_osc_coeff = osc_sync_init(o->sub_osc, buff, len);
+
+	osc_recalc_phase(o);
+
+	for (fpp_t frame = 0; frame < len; ++frame) {
+		if (osc_sync_ok(o->sub_osc, sub_osc_coeff)) {
+			o->phase = o->phase_offset;
+		}
+		buff[frame] = osc_get_sample(o, o->phase) * o->volume;
+		o->phase += osc_coeff;
+	}
+}
+
+
+// do fm by using sub-osc as modulator
+void osc_update_fm(Oscillator* o, sample_t* buff, fpp_t len) {
+	const float osc_coeff = o->freq * o->detuning;
+	const float srate_correction = 44100.0f / o->sample_rate;
+
+	osc_update(o->sub_osc, buff, len);
+	osc_recalc_phase(o);
+
+	for (fpp_t frame = 0; frame < len; ++frame) {
+		o->phase += buff[frame] * srate_correction;
+		buff[frame] = osc_get_sample(o, o->phase) * o->volume;
+		o->phase += osc_coeff;
+	}
+}
+
+
+static void
+osc_update(Oscillator* o, sample_t* buff, fpp_t len) {
+	if (o->freq >= o->sample_rate / 2) {
+		return;
+	}
+	if (o->sub_osc != NULL) {
+		switch ((int)o->modulation_algo) {
+			case OSC_MOD_PM:
+				osc_update_pm(o, buff, len);
+				break;
+			case OSC_MOD_AM:
+				osc_update_am(o, buff, len);
+				break;
+			case OSC_MOD_MIX:
+				osc_update_mix(o, buff, len);
+				break;
+			case OSC_MOD_SYNC:
+				osc_update_sync(o, buff, len);
+				break;
+			case OSC_MOD_FM:
+				osc_update_fm(o, buff, len);
+				break;
+			default:
+				fprintf(stderr, "Oscillator: Invalid modulation algorithm\n");
+		}
+	} else {
+		osc_update_no_sub(o, buff, len);
+	}
+}
+
+
+static sample_t
+osc_get_sample(Oscillator* o, float sample) {
+	switch ((int)(o->wave_shape)) {
+		case OSC_WAVE_SINE:
+			return osc_sample_sin(sample);
+		case OSC_WAVE_TRIANGLE:
+			return osc_sample_triangle(sample);
+		case OSC_WAVE_SAW:
+			return osc_sample_saw(sample);
+		case OSC_WAVE_SQUARE:
+			return osc_sample_square(sample);
+		case OSC_WAVE_MOOG:
+			return osc_sample_moog_saw(sample);
+		case OSC_WAVE_EXPONENTIAL:
+			return osc_sample_exp(sample);
+		case OSC_WAVE_NOISE:
+			return osc_sample_noise(sample);
+		default:
+			fprintf(stderr, "Oscillator: Invalid wave shape\n");
+	}
+}
---
+++ src/oscillator.h
@@ -0,0 +1,145 @@
+/*
+ * Oscillator.h - declaration of class Oscillator
+ *
+ * Copyright (c) 2004-2009 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of Linux MultiMedia Studio - http://lmms.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef OSCILLATOR_H__
+#define OSCILLATOR_H__
+
+#include <stdlib.h>
+
+#include "lmms_lv2.h"
+
+enum WaveShapes {
+	OSC_WAVE_SINE,
+	OSC_WAVE_TRIANGLE,
+	OSC_WAVE_SAW,
+	OSC_WAVE_SQUARE,
+	OSC_WAVE_MOOG,
+	OSC_WAVE_EXPONENTIAL,
+	OSC_WAVE_NOISE
+} ;
+
+
+enum ModulationAlgos {
+	OSC_MOD_PM,
+	OSC_MOD_AM,
+	OSC_MOD_MIX,
+	OSC_MOD_SYNC,
+	OSC_MOD_FM
+};
+
+
+struct Oscillator_st {
+	// Shared by all oscillators of the group
+	float wave_shape;
+	float modulation_algo;
+	// This oscillator's config
+	float freq;
+	float detuning;
+	float volume;
+	float ext_phase_offset;
+	// State
+	struct Oscillator_st* sub_osc;
+	float phase_offset;
+	float phase;
+	// TODO: const sampleBuffer * m_userWave;
+	float sample_rate;
+};
+typedef struct Oscillator_st Oscillator;
+
+
+// Public interface
+
+static Oscillator* osc_create(
+		float wave_shape, float modulation_algo,
+		float freq, float detuning, float volume,
+		struct Oscillator_st* sub_osc, float phase_offset,
+		float sample_rate);
+
+static void        osc_destroy(Oscillator* o);
+
+static void        osc_update(Oscillator* o, sample_t* buff, fpp_t len);
+static sample_t    osc_get_sample(Oscillator* o, float sample);
+
+
+// Waveform sample routines
+
+static inline sample_t
+osc_sample_sin(const float _sample) {
+	return sinf( _sample * M_PI * 2.0f );
+}
+
+static inline sample_t
+osc_sample_triangle(const float _sample) {
+	const float ph = fraction( _sample );
+	if( ph <= 0.25f )
+	{
+		return ph * 4.0f;
+	}
+	else if( ph <= 0.75f )
+	{
+		return 2.0f - ph * 4.0f;
+	}
+	return ph * 4.0f - 4.0f;
+}
+
+static inline sample_t
+osc_sample_saw(const float _sample) {
+	return -1.0f + fraction( _sample ) * 2.0f;
+}
+
+static inline sample_t
+osc_sample_square(const float _sample) {
+	return ( fraction( _sample ) > 0.5f ) ? -1.0f : 1.0f;
+}
+
+static inline sample_t
+osc_sample_moog_saw(const float _sample) {
+	const float ph = fraction( _sample );
+	if( ph < 0.5f )
+	{
+		return -1.0f + ph * 4.0f;
+	}
+	return 1.0f - 2.0f * ph;
+}
+
+static inline sample_t
+osc_sample_exp(const float _sample) {
+	float ph = fraction( _sample );
+	if( ph > 0.5f )
+	{
+		ph = 1.0f - ph;
+	}
+	return -1.0f + 8.0f * ph * ph;
+}
+
+static inline sample_t
+osc_sample_noise(const float _sample) {
+	// Precise implementation
+	// return 1.0f - rand() * 2.0f / RAND_MAX;
+
+	// Fast implementation
+	return 1.0f - fast_rand() * 2.0f / FAST_RAND_MAX;
+}
+
+#endif
